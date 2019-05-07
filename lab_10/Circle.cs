aa722837e6613f7b1db10f6f9e0024340371ec82
Andre Watts <andrewattsii@gmail.com>
2019-05-07 07:06:48 -0400

    lab_10

---
+++ lab_10/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_10
+{
+    class Circle
+    {
+
+        private double Radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double CaclculatorCircumfrance()
+        {
+           return  2 * Math.PI * Radius;
+        }
+        public double CalculatorArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+
+
+    }
+}
---
+++ lab_10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            bool yesno = true;
+            while (yesno == true)
+            {
+                Console.Write("Welcome to the circle tester \nEnter a radius :");
+
+
+                double radius = double.Parse(Console.ReadLine());
+                Circle c = new Circle(radius);
+
+                Console.WriteLine($"Circumfrance is : {c.CaclculatorCircumfrance()}");
+                Console.WriteLine($"Area is : {c.CalculatorArea()} ");
+                count++;
+                Console.Write("Continue? y/n :");
+                string response = Console.ReadLine();
+                if(response.ToLower() == "n")
+                {
+                    yesno = false;
+                }
+            }
+            Console.WriteLine($"Goodbye. You created {count} Circle object(s).");
+        }
+    }
+}
